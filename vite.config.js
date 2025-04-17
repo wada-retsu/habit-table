c88ec371d38567bd30f3b02298d53092ace3e48e
しゅうぞう <sanjiguitar02@gmail.com>
2025-04-18 04:51:33 +0900

    yarinaosi

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/habit-tracker/',
+  base: '/habit-table/',
   build: {
     outDir: 'docs',
   },
